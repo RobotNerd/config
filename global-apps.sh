a54b036f882d97fb02ab32face0390c6824689d3
Marshall Bowles <marshall.bowles@gmail.com>
2021-04-05 15:42:03 -0700

    Add apps docker, slack, veracrypt, zoom

--- global-apps.sh
+++ global-apps.sh
@@ -2,9 +2,12 @@
 
 # Applications installed on all systems.
 declare -a GLOBAL_APPS=(
+  docker
   inkscape
+  gimp
   git
   nethack
   tmux
+  veracrypt
   vim
 )
--- platforms/manjaro-xfce.sh
+++ platforms/manjaro-xfce.sh
@@ -7,7 +7,8 @@ declare -a DISTRO_APPS=(
 )
 
 declare -a SNAP_APPS=(
-  "code --classic"
+  "code --classic"    # Visual Studio Code
+  "slack --classic"
 )
 
 PKG_MGR="pacman -Syu --noconfirm"
@@ -22,6 +23,15 @@ custom_actions() {
   do
     sudo snap install $i
   done
+
+  # Download and install Zoom
+  ZOOM_PKG=zoom_x86_64.pkg.tar.xz
+  ZOOM_LOCAL=$HOME/Downloads/$ZOOM_PKG
+  ZOOM_URI=https://zoom.us/client/latest/$ZOOM_PKG
+  if [ ! -f "$ZOOM_LOCAL" ]; then
+    wget $ZOOM_URI -O $ZOOM_LOCAL
+    sudo pacman -U --noconfirm $ZOOM_LOCAL
+  fi
 }
 
 custom_teardown() {
