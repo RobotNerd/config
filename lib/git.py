802d2b1532183efea329100fc31e8bd0ca824fa4
Marshall Bowles <marshall.bowles@gmail.com>
2022-08-20 09:18:17 -0700

    fix git commands

--- lib/git.py
+++ lib/git.py
@@ -12,10 +12,10 @@ class Git:
         logger.info('configuring git')
         base_cmd = 'git config --global'.split(' ')
 
-        email = cfg['user_info']['email'].split(' ')
-        name = cfg['user_info']['name'].split(' ')
+        email = cfg['user_info']['email']
+        name = cfg['user_info']['name']
 
-        Cmd.run(base_cmd + f'user.email {email}')
-        Cmd.run(base_cmd + f'user.name {name}')
+        Cmd.run(base_cmd + f'user.email {email}'.split(' '))
+        Cmd.run(base_cmd + f'user.name {name}'.split(' '))
         Cmd.run(base_cmd + 'pager.branch false'.split(' '))
         Cmd.run(base_cmd + 'pager.diff false'.split(' '))
