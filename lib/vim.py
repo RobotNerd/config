e6ad842ea0bd1db978973e1e9f328945d2ace567
Marshall Bowles <marshall.bowles@gmail.com>
2022-08-20 09:19:38 -0700

    check vim install before installing vundle

--- lib/vim.py
+++ lib/vim.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from lib.command import Cmd
+from lib.command import Cmd, CommandRunnerError
+import os
 
 
 class Vim:
@@ -8,8 +9,26 @@ class Vim:
     def vundle(logger, cfg):
         if not cfg['vundle']:
             return
-        
+
+        if not Vim._is_installed():
+            return
+
         logger.info('installing vundle')
-        Cmd.run('rm -fr ~/.vim/bundle'.split(' '))
-        Cmd.run('git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim'.split(' '))
-        Cmd.run('vim +PluginInstall +qall'.split(' '))
+
+        remove_bundle_dir = 'rm -fr $HOME/.vim/bundle'
+        install_vundle = 'git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim'
+        install_plugins = 'vim +PluginInstall +qall'
+
+        Cmd.run(os.path.expandvars(remove_bundle_dir).split(' '))
+        Cmd.run(os.path.expandvars(install_vundle).split(' '))
+        Cmd.run(install_plugins.split(' '))
+
+    @staticmethod
+    def _is_installed():
+        installed = False
+        try:
+            Cmd.run(f'which vim'.split(' '))
+            installed = True
+        except CommandRunnerError:
+            pass
+        return installed
